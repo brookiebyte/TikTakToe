0e4823a3fdfb81dfb3a43fed55c06a6f6b261ac4
brookiebyte <brookie.darby@gmail.com>
2024-09-02 17:22:52 -0600

    GridSpace to Element set up

--- play_tik_tak_toe.py
+++ play_tik_tak_toe.py
@@ -15,6 +15,28 @@ def SwitchTurns():
         print("O")
         isXTurn = True
 
+def ConvertGridToIndex(gridSpace):
+    if gridSpace == 1:
+        indexElement = 1
+    if gridSpace == 2:
+        indexElement = 5
+    if gridSpace == 3:
+        indexElement = 9
+    if gridSpace == 4:
+        indexElement = 13
+    if gridSpace == 5:
+        indexElement = 17
+    if gridSpace == 6:
+        indexElement = 21
+    if gridSpace == 7:
+        indexElement = 25
+    if gridSpace == 8:
+        indexElement = 29
+    if gridSpace == 9:
+        indexElement = 33
+    return indexElement
+   
+#print(grid[33])
 
 # Main
 print(grid)
